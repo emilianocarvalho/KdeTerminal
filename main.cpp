5a2b75e40df095d3c508a92ba5e7898b97b04a0b
Emiliano Carvalho <emilianojpa@gmail.com>
2014-01-23 21:06:57 -0200

    Refatorando main para funções selecionáveis

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char **argv) {
-  
+void diversos (void) {
+    
     int res = 0, num1 = 5, num2 = 2, vlr;
 
     float vlr2;
@@ -41,5 +43,87 @@ int main(int argc, char **argv) {
     cout << "S = Pi * raio² -> " << s << endl;
     cout << "p = 2 * pi * raio -> " << p << endl;
         
-    return 0;
 }
+
+void meses (void){
+    int mes;
+    cout << "Meses do ano ||||||||||>" << endl;
+    cout << "digite de 1 a 12 ||||||>" << endl;
+    cin >> mes;
+
+    switch (mes) {
+    case 1:
+       cout << "Janeiro" << endl;
+       break;
+    case 2:
+       cout << "Fevereiro" << endl;
+       break;
+    case 3:
+       cout << "Março" << endl;
+       break;
+    case 4:
+       cout << "Abril" << endl;
+       break;
+    case 5:
+       cout << "Maio" << endl;
+       break;
+    case 6:
+       cout << "Junho" << endl;
+       break;
+    case 7:
+       cout << "Julho" << endl;
+       break;
+    case 8:
+       cout << "Agosto" << endl;
+       break;
+    case 9:
+       cout << "Setembro" << endl;
+       break;
+    case 10:
+       cout << "Outubro" << endl;
+       break;
+    case 11:
+       cout << "Novembro" << endl;
+       break;
+    case 12:
+       cout << "Dezembro" << endl;
+       break;
+    default:
+        cout << "digite de 1 a 12"  << endl;
+    }        
+}
+
+int main(int argc, char *argv[])
+{
+    int opcmnu;
+ 
+    while (opcmnu>0) {
+
+        cout << "Selecione a rotina |||||||||||||" << endl;
+        cout << "0 Sair             |||||||||||||" << endl;
+        cout << "1 Diversos         |||||||||||||" << endl;
+        cout << "2 Meses do Ano     |||||||||||||" << endl;
+    
+        cin >> opcmnu;
+    
+        switch (opcmnu) {
+          case 1:
+               diversos();
+               break;
+          case 2:
+               meses();
+               break;
+          case 0:
+               break;
+          default:
+               cout << "digite de 1/2 ou 0 para sair"  << endl;
+               
+        }     
+        system("PAUSE");
+        system("cls");        
+
+    }
+    
+    return EXIT_SUCCESS;
+}
+
